a9aec3abc8a21ee1e6c28fa7aec72bff6238ef8e
Brandi L Mason <masonbrandi66@gmail.com>
2024-02-13 20:37:33 -0700

    day 1 completed

---
+++ Day1/band_generator.py
@@ -0,0 +1,7 @@
+print("Hello! Thanks for using the band generator! We're here to help!")
+
+city = input("What's the name of city you grew up in?\n")
+
+pet = input("What is the name of a pet?\n")
+
+print("Your band name coule be " + city + " " + pet)
